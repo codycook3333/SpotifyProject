c1300905ec6f54310a8e5780d6e0b7ba32bd3ff7
codycook3333 <87494180+codycook3333@users.noreply.github.com>
2022-03-28 22:15:07 -0700

    Got Tkinter app working
    Created basic layout and returns unfiltered results

--- MySpotifyApp/SpotifyConnection.py
+++ MySpotifyApp/SpotifyConnection.py
@@ -1,28 +1,39 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-auth_manager = SpotifyClientCredentials()
+
+username = 12147687832
+## test username is 12147687832
+
+SPOTIPY_CLIENT_ID='7372aad238034bcd80dabd1c3925a2d0'
+SPOTIPY_CLIENT_SECRET='e690456872084adc93289a2175acd86c'
+SPOTIPY_REDIRECT_URI='http://localhost:1410/'
+
+auth_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
-username = get_username()
-
-
+spot = spotipy.Spotify()
+userPlaylists = ''
 playlists = sp.user_playlists(username)
 while playlists:
     for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+        userPlaylists += "%4d %s\n" % (i + 1 + playlists['offset'], playlist['name'])
     if playlists['next']:
         playlists = sp.next(playlists)
     else:
         playlists = None
-# export SPOTIPY_CLIENT_ID='7372aad238034bcd80dabd1c3925a2d0'
-# export SPOTIPY_CLIENT_SECRET='e690456872084adc93289a2175acd86c'
-# export SPOTIPY_REDIRECT_URI='http://localhost:1410/'
+
+# print(sp.search(q="green day", limit=20))
+# set SPOTIPY_CLIENT_ID='7372aad238034bcd80dabd1c3925a2d0'
+# set SPOTIPY_CLIENT_SECRET='e690456872084adc93289a2175acd86c'
+# set SPOTIPY_REDIRECT_URI='http://localhost:1410/'
+SelectionOptions = ["Artist", "Album", "Track Name", "Genre"]
 
 #def get_username:
 
 
-#def get_artist:
-
+def get_artist(artistName):
+    result = sp.search(q="Green Day")
+    return result
 #def get_track:
 
 #def get_album:
--- MySpotifyApp/tinkerApp.py
+++ MySpotifyApp/tinkerApp.py
@@ -1,9 +1,59 @@
+from sys import maxsize
+from tkinter import *
+from tkinter import ttk
 import tkinter as tk
-#import SpotifyConnection as SC
+from unittest import result
+import SpotifyConnection as SC
 
 #TK_SILENCE_DEPRECATION=1
 
 
-window = tk.Tk()
+# sets the username so that we can connect to thier spotify
+def set_username():
+    SC.username = usernameText.get()
+    # print(SC.username)
+    SC.connect()
 
-#print("This is somehow working")
+def searchCommand():
+    if selectionBox.current() == 0:
+        
+        searchResults = tk.Text(root)
+        searchResults.grid(row=15, column=0, pady=15)
+        searchResults.insert('1.0', SC.get_artist(searchText.get()))
+
+root = Tk()
+root.columnconfigure(0, minsize=100)
+root.columnconfigure(1, minsize=100)
+root.columnconfigure(2, minsize=100)
+root.columnconfigure(3, minsize=100)
+usernameText = tk.StringVar()
+searchText = tk.StringVar()
+selection = tk.StringVar()
+frm = ttk.Frame(root, padding=10)
+
+root.geometry("800x800")
+frm.grid()
+ttk.Label(frm, text="Enter Username to access your Spotify information.").grid(column=0, row=0, padx=5, pady=5)
+usernameEntry = ttk.Entry(frm, textvariable=usernameText, show="*").grid(column=1, row=0, padx=5, pady=5)
+#usernameEntry.focus()
+
+ttk.Button(frm, text=" Set Username and Connect ", command=set_username).grid(column=2, row=0, padx=5, pady=5)
+
+
+
+# this is the dropdown menu to specify what to search for
+
+ttk.Label(frm, text="What are you searching for?", anchor="e").grid(column=0, row=3, padx=5, pady=5)
+
+selectionBox = ttk.Combobox(frm, textvariable=selection, values=SC.SelectionOptions)
+selectionBox.grid(column=1, row=3, padx=5, pady=5)
+selectionBox.current(0)
+
+ttk.Label(frm, text="Name of what you are searching for", anchor="e").grid(column=0, row=6, padx=5, pady=5)
+searchEntry = ttk.Entry(frm, textvariable=searchText).grid(column=1, row=6, padx=5, pady=5)
+ttk.Button(frm, text=" Search ", command=searchCommand).grid(column=2, row=6, padx=5, pady=5)
+
+
+#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=15, row=15)
+root.mainloop()
+
