1eb24842e9e2a8fe9855f75c2ca88826c48faf90
codycook3333 <87494180+codycook3333@users.noreply.github.com>
2022-04-04 13:22:05 -0700

    Still working it

--- MySpotifyApp/SpotifyConnection.py
+++ MySpotifyApp/SpotifyConnection.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
+from pandastable import *
+import pandas as pd
 
 username = 12147687832
 ## test username is 12147687832
@@ -11,7 +12,6 @@ SPOTIPY_REDIRECT_URI='http://localhost:1410/'
 
 auth_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
-spot = spotipy.Spotify()
 userPlaylists = ''
 playlists = sp.user_playlists(username)
 while playlists:
@@ -21,19 +21,72 @@ while playlists:
         playlists = sp.next(playlists)
     else:
         playlists = None
+#print(userPlaylists)
 
-# print(sp.search(q="green day", limit=20))
-# set SPOTIPY_CLIENT_ID='7372aad238034bcd80dabd1c3925a2d0'
-# set SPOTIPY_CLIENT_SECRET='e690456872084adc93289a2175acd86c'
-# set SPOTIPY_REDIRECT_URI='http://localhost:1410/'
 SelectionOptions = ["Artist", "Album", "Track Name", "Genre"]
 
 #def get_username:
 
 
 def get_artist(artistName):
-    result = sp.search(q="Green Day")
-    return result
+    result = sp.search(q='artist:' + artistName, type='artist')
+    items=result['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+    newResults = sp.artist(artist['id'])
+    # d = {'Artist Name': [newResults['name']], 'Total Followers': [newResults['followers']['total']], 'Genres': [newResults['genres']], 'ID': [newResults['id']], 'Popularity': [newResults['popularity']]}
+    # df = pd.DataFrame(data=d)
+    filteredResults = []
+    filteredResults.append(newResults['name'])
+    filteredResults.append(newResults['followers']['total'])
+    filteredResults.append(newResults['genres'])
+    filteredResults.append(newResults['id'])
+    filteredResults.append(newResults['popularity'])
+    colNames = []
+    colNames = ['Artist Name: ', 'Total Followers: ', 'Genres: ', 'ID: ', 'Popularity: ']
+    df = pd.DataFrame(data=filteredResults, index=colNames)
+    df.columns = ['']
+    return df
+
+    
+
+def recommendation(info, boxPlace):
+    if boxPlace == 0:
+        # we need to find the Genre of the artist
+        result = sp.search(q='artist:' + info, type='artist')
+        items=result['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+        newResults = sp.artist(artist['id'])
+        artist_id = newResults['id']
+
+    elif boxPlace ==1:
+        #we need to find the Artist of the Album
+        # We first need the album id
+        allinfo = sp.search(q='album:'+ info, type='album')
+        albumID = allinfo['albums']['items'][0]['id']
+        #then we use that to get the artist id 
+        result = sp.album(albumID)
+        artist_id = result['artists'][0]['id']
+        
+
+    # elif boxPlace == 2:
+    #     # we need to find the Artist id of the track name
+        
+        
+
+    # # #else:
+    # #     # we are already given the Genre so begin the search.
+    # return sp.artist_related_artists(artist_id)
+    # get_genre()
+
+ 
+# result = sp.search(q='track:'+ 'Given Up', type='track')
+
+
+# print(result)
+# Temp artist id 4uG8q3GPuWHQlRbswMIRS6
+
 #def get_track:
 
 #def get_album:
--- MySpotifyApp/tinkerApp.py
+++ MySpotifyApp/tinkerApp.py
@@ -1,9 +1,10 @@
+from cgitb import text
 from sys import maxsize
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-from unittest import result
 import SpotifyConnection as SC
+from pandastable import Table
 
 #TK_SILENCE_DEPRECATION=1
 
@@ -14,46 +15,75 @@ def set_username():
     # print(SC.username)
     SC.connect()
 
+
 def searchCommand():
+    searchResults = tk.Text(can)
+    searchResults.config(height=7)
+    searchResults.place(relx=.5, rely=.5, anchor=CENTER)
     if selectionBox.current() == 0:
-        
-        searchResults = tk.Text(root)
-        searchResults.grid(row=15, column=0, pady=15)
         searchResults.insert('1.0', SC.get_artist(searchText.get()))
+        # searchResults= tk.Label(frm2, text=SC.get_artist(searchText.get())).grid(row=1, column=0, pady=15, columnspan=6)
+        # df = SC.get_artist(searchText.get())
+        # artName = ttk.Label(frm2, text=df.iloc[0]).grid(row=1, column=0, pady=15)
+    # elif selectionBox.current() == 1:
+    #     searchResults.insert('1.0', SC.get_artist(searchText.get()))
+
+    # elif selectionBox.current() == 2:
+    #     searchResults.insert('1.0', SC.get_artist(searchText.get()))
+
+    # elif selectionBox.current() == 3:
+    #     searchResults.insert('1.0', SC.get_artist(searchText.get()))
+    recommendArtist()
+
+def recommendArtist():
+    recommendBox = tk.Text(can2)
+    recommendBox.config(height=12)
+    recommendBox.place(relx=.5, rely=.5, anchor=CENTER)
+    recommendBox.insert('1.0', 'Other users also liked: \n\n ' + SC.recommendation(searchText.get(), selectionBox.current()))
+
 
 root = Tk()
-root.columnconfigure(0, minsize=100)
-root.columnconfigure(1, minsize=100)
-root.columnconfigure(2, minsize=100)
-root.columnconfigure(3, minsize=100)
+#------------------------------------------------------------------------------------------------------------------
+# root.columnconfigure(0, minsize=1000)
+# root.columnconfigure(1, minsize=100)
+# root.columnconfigure(2, minsize=100)
+# root.columnconfigure(3, minsize=100)
 usernameText = tk.StringVar()
 searchText = tk.StringVar()
 selection = tk.StringVar()
-frm = ttk.Frame(root, padding=10)
+frm = Canvas(root, bg='red', height=200, width=800)
+frm.place(relx=0.5, rely=0.2, anchor=CENTER)
+can = Canvas(root, bg='green', height=200, width=800)
+can.place(relx=0.5, rely=0.5, anchor=CENTER)
+root.geometry("1000x800")
+can2 = Canvas(root, bg='blue', height=200, width=800)
+can2.place(relx=0.5, rely=0.8, anchor=CENTER)
+#frm.grid()
+#frm2.grid()
 
-root.geometry("800x800")
-frm.grid()
-ttk.Label(frm, text="Enter Username to access your Spotify information.").grid(column=0, row=0, padx=5, pady=5)
-usernameEntry = ttk.Entry(frm, textvariable=usernameText, show="*").grid(column=1, row=0, padx=5, pady=5)
-#usernameEntry.focus()
+# Username entry for persoanl user playlists
 
-ttk.Button(frm, text=" Set Username and Connect ", command=set_username).grid(column=2, row=0, padx=5, pady=5)
+# ttk.Label(frm, text="Enter Username to access your Spotify information.").grid(column=0, row=0, padx=5, pady=5)
+# usernameEntry = ttk.Entry(frm, textvariable=usernameText, show="*").grid(column=1, row=0, padx=5, pady=5)
+# ttk.Button(frm, text=" Set Username and Connect ", command=set_username).grid(column=2, row=0, padx=5, pady=5)
 
 
 
-# this is the dropdown menu to specify what to search for
+# Dropdown menu to specify what to search for
 
-ttk.Label(frm, text="What are you searching for?", anchor="e").grid(column=0, row=3, padx=5, pady=5)
-
+ttk.Label(frm, text="What are you searching for?").place(relx=0.2, rely=0.3, anchor=CENTER)
 selectionBox = ttk.Combobox(frm, textvariable=selection, values=SC.SelectionOptions)
-selectionBox.grid(column=1, row=3, padx=5, pady=5)
+selectionBox.place(relx=0.8, rely=0.3, anchor=CENTER)  
 selectionBox.current(0)
 
-ttk.Label(frm, text="Name of what you are searching for", anchor="e").grid(column=0, row=6, padx=5, pady=5)
-searchEntry = ttk.Entry(frm, textvariable=searchText).grid(column=1, row=6, padx=5, pady=5)
-ttk.Button(frm, text=" Search ", command=searchCommand).grid(column=2, row=6, padx=5, pady=5)
+# Search box to enter in your search results
+
+ttk.Label(frm, text="Name of what you are searching for").place(relx=0.2, rely=0.6, anchor=CENTER)  
+searchEntry = ttk.Entry(frm, textvariable=searchText).place(relx=0.8, rely=0.6, anchor=CENTER) 
+ttk.Button(frm, text=" Search ", command=searchCommand).place(relx=0.5, rely=0.8, anchor=CENTER) 
 
 
 #ttk.Button(frm, text="Quit", command=root.destroy).grid(column=15, row=15)
+#----------------------------------------------------------------------------------------------------------------------
 root.mainloop()
 
